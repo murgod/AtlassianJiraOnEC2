20e11f40a91a759451dc8ef0d0a59269a22d31b5
murgod <murgod.a@husky.neu.edu>
2020-02-06 14:56:12 -0500

    Final commit

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 This repository is to deploy Atlassian Jira on AWS EC2 instance with Automated script using Shell, AWS cloudFormation and Ansible
 
 #How to run
-aws cloudformation create-stack --template-body file://SetupEC2Instance.yml --stack-name ec2Test1 --parameters ParameterKey=KeyName,ParameterValue=provEC2 ParameterKey=InstanceType,ParameterValue=t2.micro
+aws cloudformation create-stack --template-body file://Template/SetupEC2Instance.yml --stack-name ec2Test1 --parameters ParameterKey=KeyName,ParameterValue=provEC2 ParameterKey=InstanceType,ParameterValue=t2.micro
--- aws_create_stack.sh
+++ aws_create_stack.sh
@@ -1,28 +1,19 @@
 echo "Enter the stack name to be created"
 read sName
 
-	mVPCSubnetCidrBlock=10.0.0.0/16
-	mAvailabilityZone1=a
-	mAvailabilityZone2=b
-	mAvailabilityZone3=c
-	mAvailabilityZone4=d
-	mSubnetCidrBlock1=10.0.1.0/24
-	mSubnetCidrBlock2=10.0.2.0/24
-	mSubnetCidrBlock3=10.0.3.0/24
-	mSubnetCidrBlock4=10.0.4.0/24
-	mRouteIpaddress=0.0.0.0/0
-	StackName=$sName
+StackName=$sName
 
-	echo "$sName Stack creation in progress..."
+echo "$sName Stack creation in progress..."
 
-	stackID=$(aws cloudformation create-stack --stack-name $sName --template-body file://Template/SetupEC2.yml)
-	aws cloudformation wait stack-create-complete --stack-name $stackID
+stackID=$(aws cloudformation create-stack --stack-name $sName --template-body file://Templates/SetupEC2.yml)
+aws cloudformation wait stack-create-complete --stack-name $sName
 
-	echo $stackID
+echo $stackID
 
-	if [ -z $stackID ]; then
-		echo 'Error. Stack creation failed !!!'
-		exit 1
-	else
-		echo "Stack Creation Done !!!"
-	fi
+if [ -z $stackID ]; then
+	echo 'Error. Stack creation failed !!!'
+	exit 1
+else
+	echo "Stack Creation Done !!!"
+	#Ansible script can be run here.
+fi
